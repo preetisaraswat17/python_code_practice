06e9cb6d19bbb1428dbb87714861a3f57e2bcb53
preeti saraswat <preetisaraswat17@gmail.com>
2020-12-10 01:18:10 -0800

    Update Second_largest_num.py

--- Second_largest_num.py
+++ Second_largest_num.py
@@ -1,10 +1,15 @@
+#create list
 num=int(input())
 l=[]
 for i in range(num):
     l.append(int(input()))
+
+#sort list and reverse as sort is in ascending order by default or sort(reverse=True) for descending
 l.sort()
 l.reverse()
 print(l)
+
+#compare only first few and break
 for i in range(len(l)):
     if l[i]<l[0]:
         print(f"second highest ele is {l[i]}")
